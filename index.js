7498051180ceeb52ca5ace696617a3af2aa64d6e
Ngoc Nguyen <nnpk0410@gmail.com>
2023-03-17 06:48:05 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(action="roller-skate") {
+    return `This Saturday, I want to ${action}!`;
+}
+
+
+const mondayWork = function(action="go to the office") {
+    return `This Monday, I will ${action}.`;
+}
+
+
+const wrapAdjective = function(visualFlair="*") {
+    return function(s="special") {
+        return `You are ${visualFlair}${s}${visualFlair}!`
+    }
+}
